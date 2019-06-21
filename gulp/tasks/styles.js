b0abaad4b2ad2ecb105f9ee43fb6f66334c2d255
Brandon Corey <bcorey85@gmail.com>
2019-06-21 09:14:03 -0500

    finish first section after hero

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -9,9 +9,6 @@ let gulp = require('gulp'),
 gulp.task('styles', function() {
     return gulp.src('./app/assets/styles/styles.css')
         .pipe(postcss([cssImport, mixins, cssvars, nested, autoprefixer]))
-        .on('error', function(errorInfo) {
-            console.log(errorInfo.toString);
-            this.emit('end');
-        })
+
         .pipe(gulp.dest('./app/temp/styles'));
 })
