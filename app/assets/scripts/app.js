92e781b47acda260a21f02bb4b5190a2431f9257
Brandon Corey <bcorey85@gmail.com>
2019-06-25 13:12:44 -0500

    add link highlights

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -1,6 +1,8 @@
 import MobileMenu from './modules/MobileMenu'
 import RevealOnScroll from './modules/RevealOnScroll'
+import StickyHeader from './modules/StickyHeader'
 
 let mobileMenu = new MobileMenu()
 new RevealOnScroll(document.querySelectorAll('.feature-item'), '85%')
 new RevealOnScroll(document.querySelectorAll('.testimonial'), '70%')
+let stickyHeader = new StickyHeader()
---
+++ app/assets/scripts/modules/StickyHeader.js
@@ -0,0 +1,62 @@
+import waypoints from '../../../../node_modules/waypoints/lib/noframework.waypoints'
+
+class StickyHeader {
+    constructor() {
+        this.siteHeader = document.querySelector('.site-header')
+        this.headerTriggerElement = document.querySelector('.large-hero__title')
+        this.createHeaderWaypoint()
+        this.pageSections = document.querySelectorAll('.page-section')
+        this.headerLinks = document.querySelectorAll('.primary-nav a')
+        this.createPageSectionWaypoints()
+    }
+
+    createHeaderWaypoint() {
+        let that = this
+        new Waypoint({
+            element: that.headerTriggerElement,
+            handler: function(direction) {
+                if(direction === "down") {
+                    that.siteHeader.classList.add('site-header--dark')
+                } else {
+                    that.siteHeader.classList.remove('site-header--dark')
+                }
+            }
+        }) 
+    }
+
+    createPageSectionWaypoints() {
+        let that = this
+        for(let i = 0; i < this.pageSections.length; i++) {
+            let currentItem = this.pageSections[i]
+            new Waypoint({
+                element: currentItem,
+                handler: function(direction) {
+                    if (direction === "down") {
+                        let matchingHeaderLink = currentItem.getAttribute('data-matching-link')
+                        for (let i = 0; i < that.pageSections.length; i++) {
+                            that.headerLinks[i].classList.remove('is-current-link')
+                        }
+                        document.querySelector(matchingHeaderLink).classList.add('is-current-link')
+                    }                     
+                },
+                offset: 40%
+            })
+
+            new Waypoint({
+                element: currentItem,
+                handler: function (direction) {
+                    if (direction === "up") {
+                        let matchingHeaderLink = currentItem.getAttribute('data-matching-link')
+                        for (let i = 0; i < that.pageSections.length; i++) {
+                            that.headerLinks[i].classList.remove('is-current-link')
+                        }
+                        document.querySelector(matchingHeaderLink).classList.add('is-current-link')
+                    }
+                },
+                offset: -10%
+            })
+        }
+    }
+}
+
+export default StickyHeader
