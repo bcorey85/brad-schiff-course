043b0f67376c21605e152f9f8a1844028320a2c4
Brandon Corey <bcorey85@gmail.com>
2019-06-25 23:25:36 -0500

    complete lazyloading of images

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -3,6 +3,7 @@ import RevealOnScroll from './modules/RevealOnScroll'
 import StickyHeader from './modules/StickyHeader'
 import Modal from './modules/Modal'
 
+
 let mobileMenu = new MobileMenu()
 new RevealOnScroll(document.querySelectorAll('.feature-item'), '85%')
 new RevealOnScroll(document.querySelectorAll('.testimonial'), '70%')
